fdc1ec87fc9cf6b30b6fac03c061b2b17da408d1
shengxingyue <shengxingyue@souche.com>
2017-11-28 18:07:54 +0800

    用一波java8内置的函数接口

--- java8/src/main/java/com/sheng/java8/lambda/Demo1.java
+++ java8/src/main/java/com/sheng/java8/lambda/Demo1.java
@@ -1,32 +1,19 @@
 package com.sheng.java8.lambda;
 
+import java.util.function.Function;
+
 /**
  * @author shengxingyue, created on 2017/11/28
  */
 public class Demo1 {
 
-    /**
-     * 声明一个函数式接口
-     * @param <T>
-     */
-    @FunctionalInterface
-    interface Predicate<T> {
-        boolean test(T t);
-    }
-
-    /**
-     * 使用函数式接口执行判断逻辑
-     * @param x
-     * @param predicate
-     * @return
-     */
-    public static boolean test(int x, Predicate<Integer> predicate) {
-        return predicate.test(x);
+    public static int add(int x, Function<Integer, Integer> function) {
+        return function.apply(x);
     }
 
     public static void main(String[] args) {
-        boolean isAdult = test(17, x -> x >= 18);
-        System.out.println(isAdult);
+        int result = add(10, x -> x + 20);
+        System.out.println(result);
     }
 
 }
